34b54e0c42ff8808251cc74c8ba7fb909f72cc00
Wevesson Madson <104700348+WevessonMadson@users.noreply.github.com>
2026-05-06 13:35:16 -0300

    aplica automaticamente

--- md-move-matriz.user.js
+++ md-move-matriz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Movidesk - mover chat matriz
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0.0
 // @description  Ferramenta de visualização para chat matriz no Movidesk.
 // @match        *://*.movidesk.com/*
 // @updateURL    https://raw.githubusercontent.com/WevessonMadson/userscripts/main/md-move-matriz.user.js
@@ -10,23 +10,35 @@
 
 (function() {
     'use strict';
-    document.addEventListener('keydown', function(e) {
-        if (e.altKey && (e.key === 'm' || e.key === 'M')) {
-            e.preventDefault(); e.stopPropagation();
-            tooglePositionChat();
+    'use strict';
+
+    function applyStyle() {
+        const elementChat = document.querySelector(
+            ".md-chat-widget-btn-wrapper[data-expanded='true']"
+        );
+
+        if (!elementChat) return;
+
+        const icon = elementChat.querySelector(".md-chat-widget-btn-icon");
+        const title = elementChat.querySelector(".md-chat-widget-btn-title");
+
+        if (title) {
+            title.style.display = "none";
         }
-    });
 
-
-    function tooglePositionChat() {
-        let elementChat = document.querySelector(".md-chat-widget-btn-container .md-chat-widget-btn-wrapper[data-expanded='true']");
-
-        if (elementChat.childNodes[2].style.display != "none") {
-            elementChat.childNodes[2].style.display = "none";
-            elementChat.childNodes[1].style.marginRight = "15px";
-        } else {
-            elementChat.childNodes[2].style.display = "";
-            elementChat.childNodes[1].style.marginRight = "0px";
+        if (icon) {
+            icon.style.marginRight = "15px";
         }
     }
+
+    // observa mudanças no DOM (Movidesk recria isso direto)
+    const observer = new MutationObserver(applyStyle);
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    // primeira execução
+    applyStyle();
 })();
